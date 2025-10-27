d9416d1367ad1785d276ea48a9d2e57d3c2364e6
Demetri Georgalas <dgeorgalas96@gmail.com>
2025-10-27 18:33:18 -0500

    added all convert button functions and adding to html

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const inputEl = document.getElementById("input-el")
 
 const convertBtn = document.getElementById("convert-btn")
 
+const lengthCalc = document.getElementById("length-calc")
+const volumeCalc = document.getElementById("volume-calc")
+const massCalc = document.getElementById("mass-calc")
+
 convertBtn.addEventListener("click", function() {
     convertLength(inputEl.value)
     convertVolume(inputEl.value)
@@ -9,15 +13,22 @@ convertBtn.addEventListener("click", function() {
 })
 
 function convertLength(length) {
-    newFeet = (length * 3.28084).toFixed(3)
-    newMeter = (length/3.28084).toFixed(3)
+    let newFeet = (length * 3.28084).toFixed(3)
+    let newMeter = (length/3.28084).toFixed(3)
+    lengthCalc.innerHTML = `${length} meters = ${newFeet} feet | ${length} feet = ${newMeter} meters`
     console.log(`${length} meters = ${newFeet} feet | ${length} feet = ${newMeter} meters`)
 }
 
 function convertVolume(volume) {
-
+    let newGallon = (volume*0.264172).toFixed(3)
+    let newLiter = (volume/0.264172).toFixed(3)
+    volumeCalc.innerHTML = `${volume} liters = ${newGallon} gallons | ${volume} gallons = ${newLiter} liters`
+    console.log(`${volume} liters = ${newGallon} gallons | ${volume} gallons = ${newLiter} liters`)
 }
 
 function convertMass(mass) {
-
+    let newPound = (mass*2.20462).toFixed(3)
+    let newKilo = (mass/2.20462).toFixed(3)
+    massCalc.innerHTML = `${mass} kilos = ${newPound} pounds | ${mass} pounds = ${newKilo} kilos`
+    console.log(`${mass} kilos = ${newPound} pounds | ${mass} pounds = ${newKilo} kilos`) 
 }
